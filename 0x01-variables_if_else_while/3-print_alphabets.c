08e6ce0b9fe7355dee2f2d4bc3dfe39b88ae5e1c
mkmtde47 <mkmmogano@gmail.com>
2023-02-17 13:51:23 -0800

    -mTASK 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Zero if successful
+  */
+int main(void)
+{
+	char lower[] = {'a', 'b', 'c', 'd', 'e', 'f',
+		'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+		'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+		'w', 'x', 'y', 'z'};
+	char upper[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G',
+		'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+		'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(lower[i]);
+	}
+	for (i = 0; i < 26; i++)
+	{
+		putchar(upper[i]);
+	}
+	putchar('\n');
+	return (0);
+}
