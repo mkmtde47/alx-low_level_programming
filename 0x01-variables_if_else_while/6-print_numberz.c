24bb0fbf90dfb775ce5a9f9dfac9ae9792cff5b6
mkmtde47 <mkmmogano@gmail.com>
2023-02-17 14:27:50 -0800

    -mTASK 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Zero if successful
+  */
+int main(void)
+{
+	int numbers[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(numbers[i]);
+	}
+	putchar('\n');
+	return (0);
+}
