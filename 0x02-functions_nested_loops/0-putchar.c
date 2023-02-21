81d0acfe52030d8fffc4d00b0d668817d1ecfc37
mkmtde47 <mkmmogano@gmail.com>
2023-02-21 11:12:45 -0800

    -mTask 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Zero if successful
+  */
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+This directory contains all ALX 
+tutorials on functions and loops (nested).
