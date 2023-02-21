88628efd6085ae48a39c3bc6bc3eb1acffde3ad6
mkmtde47 <mkmmogano@gmail.com>
2023-02-21 15:58:08 -0800

    -mTASK 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -11,7 +11,7 @@ void print_alphabet(void)
 	char alpha[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g'
 		, 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o'
 			, 'p', 'q', 'r', 's', 't', 'u'
-			, 'w', 'x', 'y' , 'z'};
+			, 'v', 'w', 'x', 'y', 'z'};
 	int n;
 
 	for (n = 0; n < 26; n++)
