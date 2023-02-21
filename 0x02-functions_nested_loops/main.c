6e9c322372dafd413aab59b16a7651808e755bb9
mkmtde47 <mkmmogano@gmail.com>
2023-02-21 15:22:01 -0800

    -mTASK 1

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -12,3 +12,20 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
+void print_alphabet(void)
+{
+	char alpha[] = {'a', 'b', 'c', 'd', 'e', 'f'
+                 , 'g', 'h', 'i', 'j', 'k', 'l', 'm'
+                         , 'n', 'o', 'q', 'r', 's'
+                         , 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	int n;
+
+	n = 0;
+	for (n = 0; n < 26; n++)
+	{
+		_putchar(alpha[n]);
+	}
+	_putchar('\n');
+	return (0);
+}
