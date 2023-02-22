acfbd54ca0bcd9cef09eee27954fcd7e1f1e7d69
mkmtde47 <mkmmogano@gmail.com>
2023-02-21 17:52:13 -0800

    -mTASK 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * print_sign - Prints sign of a number
+  * @n: Number to be checked
+  *
+  * Return: 1 for +, 0 for 0, -1 for -
+  */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
