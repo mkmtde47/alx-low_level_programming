d11ee241baf7b533644965fc4e96ff1b9842a754
mkmtde47 <mkmmogano@gmail.com>
2023-02-21 18:58:26 -0800

    -TASK 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * print_last_digit - Pritn last digit
+  * @a: Digit
+  *
+  * Return: Last digit
+  */
+int print_last_digit(int a)
+{
+	a = a % 10;
+	return (a);
+}
