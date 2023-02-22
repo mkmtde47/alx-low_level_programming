f80dfba21ec83a032dcaa50da60ca471eb8a2e79
mkmtde47 <mkmmogano@gmail.com>
2023-02-21 16:23:07 -0800

    -mTASK 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * int _islower - Function for checking lower case
+  * alphabets
+  *
+  * Return: One if lowercase, Zero if otherwise
+  */
+int _islower(int c)
+{
+	char alpha[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g'
+		, 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o'
+			, 'p', 'q', 'r', 's', 't', 'u'
+			, 'v', 'w', 'x', 'y', 'z'};
+	int i;
+
+	i = 0;
+	while (c != alpa[i])
+	{
+		
+	}
+
+	if (c == alpha[i])
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
