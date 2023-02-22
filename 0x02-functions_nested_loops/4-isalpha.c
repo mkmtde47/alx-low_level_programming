abcf77c4a1a9fdd157e65bb5e3e30c89a40f3d09
mkmtde47 <mkmmogano@gmail.com>
2023-02-21 16:43:29 -0800

    -mTASK 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * int _islower - Function for checking alphabets
+  *
+  * Return: One if alphabet, Zero if otherwise
+  */
+int _isalpha(int c)
+{
+	char alpha[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g'
+		, 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o'
+			, 'p', 'q', 'r', 's', 't', 'u'
+			, 'v', 'w', 'x', 'y', 'z'};
+	char alphABC[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G'
+		, 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O'
+			, 'P', 'Q', 'R', 'S', 'T', 'U'
+			, 'V', 'W', 'X', 'Y', 'Z'};
+	int i;
+
+	i = 0;
+	while (c != alpha[i] || c != alphABC[i])
+	{
+		
+	}
+
+	if (c == alpha[i] || c == alphABC[i])
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
