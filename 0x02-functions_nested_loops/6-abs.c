6f6b226eed2108d08fb755b7d4dc8fd2e91f2405
mkmtde47 <mkmmogano@gmail.com>
2023-02-21 18:37:59 -0800

    -mTASK 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include <stio.h>
+#include "main.h"
+
+/**
+  * _abs - Absolute value compute
+  * @a: Value computed
+  *
+  * Return: Absolute value
+  */
+int _abs(int a)
+{
+	if (a < 0)
+	{
+		a = a * (-1);
+	}
+	return (a);
+}
