1071959c0f209c2e04208c1ffa4b54ae305fab82
mkmtde47 <mkmmogano@gmail.com>
2023-02-21 16:02:41 -0800

    -mTASK 1

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * print_alphabet - Function for printing alphabets
+  *
+  * Return: Zero if successful
+  */
+void print_alphabet(void)
+{
+	char alpha[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g'
+		, 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o'
+			, 'p', 'q', 'r', 's', 't', 'u'
+			, 'v', 'w', 'x', 'y', 'z'};
+	int n;
+	int i;
+
+	i = 0;
+	while(i < 10)
+	{
+		for (n = 0; n < 26; n++)
+		{
+			_putchar(alpha[n]);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
