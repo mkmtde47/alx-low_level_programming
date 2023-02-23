c4f5d39d5fc5e552d856a7e0b39a76b5f3148fcb
mkmtde47 <mkmmogano@gmail.com>
2023-02-23 05:42:16 -0800

    -mTASK 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,23 +10,24 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+	int largest;
 
-    if (a > b && b > c)
-    {
-        largest = a;
-    }
-    else if (b > a && a > c)
-    {
-        largest = b;
-    }
-    else if (c > a && a > b)
-    {
-        largest = c;
-    }else
-    {
-	    continue;
-    }
+	if (a > b && b > c)
+	{
+		largest = a;
+	}
+	else if (b > a && a > c)
+	{
+		largest = b;
+	}
+	else if (c > a && a > b)
+	{
+		largest = c;
+	}
+	else
+	{
+		continue;
+	}
 
-    return (largest);
+	return (largest);
 }
