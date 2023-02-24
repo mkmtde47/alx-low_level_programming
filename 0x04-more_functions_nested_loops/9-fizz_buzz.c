f2659f1a8e3ebfae2325ef6f5132fd3bb6ca7447
mkmtde47 <mkmmogano@gmail.com>
2023-02-24 05:52:27 -0800

    -mTASK 8

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * Return: Zero if successful
+  */
+int main(void)
+{
+	char fizz[] = "Fizz";
+	char buzz[] = "Buzz";
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("%s%s ", fizz, buzz);
+		}
+		else if (i % 5 == 0)
+		{
+			printf("%s ", buzz);
+		}
+		else if (i % 3 == 0)
+		{
+			printf("%s ", fizz);
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	return (0);
+}
