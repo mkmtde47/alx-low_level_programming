95ab89e882a58ab74ee9c0d30bb03364cdfd4bf6
mkmtde47 <mkmmogano@gmail.com>
2023-02-24 02:29:13 -0800

    -mTASK 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,10 +11,17 @@ void print_square(int size)
 
 	for (j = 0; j < size; j++)
 	{
-		for (i = 0; i < size; i++)
+		if (size > 0)
 		{
-			_putchar('#');
+			for (i = 0; i < size; i++)
+			{
+				_putchar('#');
+			}
 		}
+		_putchar('\n');
+	}
+	if (size < 1)
+	{
 		_putchar('\n');
 	}
 }
