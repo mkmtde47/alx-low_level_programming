f836a7fc39c6672e0885d7a6041b658a3c553091
mkmtde47 <mkmmogano@gmail.com>
2023-02-24 09:13:45 -0800

    -mTASK 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,21 +12,21 @@ int main(void)
 
 	for (i = 1; i < 101; i++)
 	{
-		if ((i == 100) && (i % 5 ==0))
+		if (i == 100)
 		{
-			printf("buzz");
+			printf("Buzz");
 		}
 		else if ((i % 3 == 0) && (i % 5 == 0))
 		{
-			printf("fizzbuzz");
+			printf("FizzBuzz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("buzz");
+			printf("Buzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("fizz");
+			printf("Fizz");
 		}
 		else
 		{
