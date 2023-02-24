09f652b792ed65173d56eef393b464e286f144fe
mkmtde47 <mkmmogano@gmail.com>
2023-02-23 17:24:01 -0800

    -mTASK 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * numbers - Prints 0-9
+  */
+void print_numbers(void)
+{
+	int numbers[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int i;
+
+	i = 0;
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(numbers[i] + '0');
+	}
+}
