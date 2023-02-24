5642f989c0d14e382c42d88f00b7101764148fed
mkmtde47 <mkmmogano@gmail.com>
2023-02-23 17:39:32 -0800

    -mTASK 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-  * print_numbers - Prints 0-9
+  * print_most_numbers - Prints 0-9
   *
   */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int numbers[] = {0, 1, 3, 5, 6, 7, 8, 9};
 	int i;
