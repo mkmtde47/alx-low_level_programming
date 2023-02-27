6216a4b558a275176a78c2e95cfe3e74a3319cb6
mkmtde47 <mkmmogano@gmail.com>
2023-02-27 12:21:13 -0800

    -mTASK 1

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * _strln - Findsthe strings length
+  * @s: Pointer containg string
+  * Return: strln value
+  */
+int _strlen(char *s)
+{
+	int strln;
+
+	strln = strlen(s);
+	return strln;
+}
