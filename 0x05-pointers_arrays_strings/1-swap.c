38c5984b931f0f26c1d62804535bc6ab3e2e1253
mkmtde47 <mkmmogano@gmail.com>
2023-02-27 11:33:01 -0800

    -mTASK 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * swap_int - Swaps two variable's values
+  * @a: Pointer to variable to be swapped
+  * @b: Pointer to varuableto be swapped
+  */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
