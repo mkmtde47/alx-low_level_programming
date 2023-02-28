542ca917e800dabbb99e5c7bf8c8479fe5958bea
mkmtde47 <mkmmogano@gmail.com>
2023-02-28 08:42:31 -0800

    -mTASK 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+  * puts_half - Findsthe strings length
+  * @str: Pointer containg string
+  */
+void puts_half(char *str)
+{
+	int strln;
+	int i;
+
+	i = 0;
+	strln = 0;
+	while (*(str + i) != '\0')
+	{
+		if (*str == '\0')
+		{
+			break;
+		}
+		strln = i + 1;
+		i++;
+	}
+	i = strln / 2;
+	while (i < strln)
+	{
+		_putchar(*(str + i));
+		i++;
+	}
+	_putchar('\n');
+}
