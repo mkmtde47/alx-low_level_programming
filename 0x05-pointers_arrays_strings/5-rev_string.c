a62716e224fd9b63cfcc326f58a7c85ef3b647df
mkmtde47 <mkmmogano@gmail.com>
2023-02-28 08:17:09 -0800

    -mTASK 6

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * print_rev - Prints a strngi in reverse
+  * @s: String to be printed
+  */
+void rev_string(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	while (i > -1)
+	{
+		if (*(s + i) != '\0')
+		{
+			_putchar(*(s + i));
+		}
+		i--;
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -10,4 +10,6 @@ void _puts(char *str);
 void print_rev(char *s);
 int _putchar(char c);
 void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
 #endif
