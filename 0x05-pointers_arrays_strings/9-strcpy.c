b11ec8747c26a1fe3f7aad6be173dbf4ace27d29
mkmtde47 <mkmmogano@gmail.com>
2023-02-28 12:36:29 -0800

    -mTASK 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,12 +1,18 @@
 #include "main.h"
 #include <string.h>
 
+/**
+  * _strcpy - Copies string from one place to another (memory)
+  * @dest: Destination of string
+  * @src: Source of string
+  * Return: dest
+  */
 char *_strcpy(char *dest, char *src)
 {
 	int len = strlen(src);
 	int i;
 
-	for (i = 0; i < len; i++)
+	for (i = 0; i < len + 1; i++)
 	{
 		dest[i] = src[i];
 	}
