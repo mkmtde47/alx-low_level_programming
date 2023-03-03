234655cc5343a2bc5da4be60a38c0802d6d86b40
mkmtde47 <mkmmogano@gmail.com>
2023-03-03 04:20:31 -0800

    -mTASK 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -26,11 +26,7 @@ char *cap_string(char *str)
 			*(str + i) == '{' ||
 			*(str + i) == '}')
 		{
-/*			if (*(str + i) == '\t')
-                	{
-				*(str + i) = 32;
-                	}
-*/			i++;
+			i++;
 			if ((*(str + i) >= 'a') && (*(str + i) <= 'z'))
 			{
 				*(str + i) = *(str + i) - 32;
