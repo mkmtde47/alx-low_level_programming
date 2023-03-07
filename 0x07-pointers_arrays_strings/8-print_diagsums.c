c10de7a36bc738dc35248d49a6f01b789461caad
mkmtde47 <mkmmogano@gmail.com>
2023-03-07 11:39:14 -0800

    -mTASK 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -3,12 +3,12 @@
 
 void print_diagsums(int *a, int size)
 {
-  int i, j, sum1 = 0, sum2 = 0;
+  int i, sum1 = 0, sum2 = 0;
 
-  for (i = 0; i < size; i++) {
-    sum1 += a[i * size + i]; // add diagonal element from top-left to bottom-right
-    sum2 += a[i * size + (size - i - 1)]; // add diagonal element from top-right to bottom-left
+  for (i = 0; i < size; i++)
+  {
+	  sum1 += a[i * size + i];
+	  sum2 += a[i * size + (size - i - 1)];
   }
-
   printf("%d, %d\n", sum1, sum2);
 }
