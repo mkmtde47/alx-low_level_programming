1bca37847369d4ab5c0a620ee0eb78d529cb838a
mkmtde47 <mkmmogano@gmail.com>
2023-03-07 11:55:58 -0800

    -mTASK 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,28 +8,28 @@
   */
 void print_diagsums(int *a, int size)
 {
-        int i;
-        int j;
-        int diagsum;
+	int i;
+	int j;
+	int diagsum;
 	int diagsum2;
 
-        diagsum = 0;
+	diagsum = 0;
 	diagsum2 = 0;
-        i = 0;
-        j = 0;
-        while (i < size)
-        {
-                diagsum = diagsum + a[i * size + j]; 
-                j++;
-                i++;
-        }
-        i = 0;
-        j = 0;
-        while (i < size)
-        {
-                diagsum2 = diagsum2 + a[i * size + (size - j - 1)];
-                j++;
-                i++;
-        }
-        printf("%d, %d\n", diagsum, diagsum2);
+	i = 0;
+	j = 0;
+	while (i < size)
+	{
+		diagsum = diagsum + a[i * size + j];
+		j++;
+		i++;
+	}
+	i = 0;
+	j = 0;
+	while (i < size)
+	{
+		diagsum2 = diagsum2 + a[i * size + (size - j - 1)];
+		j++;
+		i++;
+	}
+	printf("%d, %d\n", diagsum, diagsum2);
 }
