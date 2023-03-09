6488dc945c91a6c09d3e3f3f2f4c6713c0afa29e
mkmtde47 <mkmmogano@gmail.com>
2023-03-09 08:39:45 -0800

    TASK 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,9 +7,16 @@
   */
 int factorial(int n)
 {
-	if (n <= 1)
+	if (n == 0)
 	{
 		return (1);
 	}
-	return (n * factorial(n - 1));
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
 }
