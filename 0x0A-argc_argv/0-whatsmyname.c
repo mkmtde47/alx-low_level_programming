ce52f8b6ac300613ce9b3da7f11446758bcc908d
mkmtde47 <mkmmogano@gmail.com>
2023-03-12 05:48:38 -0700

    TASK 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * main - Main function, entry point
+  * @argc: Unused variables
+  * @argv: Pointer to a pointer of arrays
+  * Return: Zero if successful
+  */
+int main(int __attribute__ ((unused)) argc, char **argv)
+{
+	printf("%s", argv[0]);
+	printf("\n");
+	return (0);
+}
