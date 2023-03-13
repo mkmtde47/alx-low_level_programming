d7422caa602e3bbc99368bdfc03aaf5447332772
mkmtde47 <mkmmogano@gmail.com>
2023-03-13 15:35:32 -0700

    TASK 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,12 +10,20 @@ int **alloc_grid(int width, int height)
 {
 	int **arr;
 	int i;
+	int j;
 
 	arr = malloc(height * sizeof(int));
 	for (i = 0; i < height; i++)
 	{
 		arr[i] = malloc(width * sizeof(int));
-		arr[i] = 0;
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			arr[i][j] = 0;
+		}
 	}
 	return (arr);
 }
