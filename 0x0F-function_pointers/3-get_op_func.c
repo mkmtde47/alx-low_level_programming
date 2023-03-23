0a0e3a1f8b834b668447b936dfb268f4bdea2c56
mkmtde47 <mkmmogano@gmail.com>
2023-03-23 15:59:13 -0700

    TASK 3

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+
+/**
+  * get_op_func - Pointer to a function
+  * @s: Pointer to char
+  * @: Integer
+  * @: Interger
+  * Return: Integer if successful
+  */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (ops[i].op != NULL)
+	{
+		if (*(ops[i].op) == *s)
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+  * op_add - Add function
+  * @a: Integer 1
+  * @b: Integer 2
+  * Return: Answer
+  */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+  * op_sub - sub function
+  * @a: Integer 1
+  * @b: Integer 2
+  * Return: Answer
+  */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+  * op_mul - mul function
+  * @a: Integer 1
+  * @b: Integer 2
+  * Return: Answer
+  */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+  * op_div - div function
+  * @a: Integer 1
+  * @b: Integer 2
+  * Return: Answer
+  */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+  * op_mod - mod function
+  * @a: Integer 1
+  * @b: Integer 2
+  * Return: Answer
+  */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
