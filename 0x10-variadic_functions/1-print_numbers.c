0478281c1915ef88b69d074a7677891d3ad15eab
mkmtde47 <mkmmogano@gmail.com>
2023-03-24 12:55:22 -0700

    TASK 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,7 +21,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 			continue;
 		}
 		printf("%d", va_arg(argptr, int));
-		if (i == n -1)
+		if (i == n - 1)
 		{
 			break;
 		}
