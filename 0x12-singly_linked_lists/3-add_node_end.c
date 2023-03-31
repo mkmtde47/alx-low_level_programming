403de664605f5e82aaa98370ca38b6739d4ad034
mkmtde47 <mkmmogano@gmail.com>
2023-03-31 05:27:33 -0700

    TASK 3

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+  * add_node_end - Function that returns a pointer to a node
+  * @head: Pointer to a node
+  * @str: Pointer to a string
+  * Return: Address of new element
+  */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new;
+	list_t *current;
+
+	/*head = NULL;*/
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->next = NULL;
+	if(*head == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		current = *head;
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+	}
+	current->next = new;
+
+	return (new);
+}
