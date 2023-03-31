136f42cbe5c44bd52b0f633bc136bbe9c8c68184
mkmtde47 <mkmmogano@gmail.com>
2023-03-31 06:11:58 -0700

    TASK 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+  * fre_list - A function that frees memory
+  * @head: Pointer to a node
+  */
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	current = head;
+	while (current != NULL)
+	{
+		current = current->next;
+		free(head);
+		head = current;
+	}
+}
