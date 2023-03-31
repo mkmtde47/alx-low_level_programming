b5686b7ffdaa127b96cb1dd16342f2b4f112d32b
mkmtde47 <mkmmogano@gmail.com>
2023-03-31 05:46:00 -0700

    TASK 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -12,7 +12,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *current;
 
 	/*head = NULL;*/
-	new = malloc(sizeof(list_t));
+	new = (list_t*)malloc(sizeof(list_t));
 	if (new == NULL || str == NULL)
 	{
 		free(new);
