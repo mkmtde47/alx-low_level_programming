5fba70758c9066d3d6a6ea91a557e5fcf54031ca
mkmtde47 <mkmmogano@gmail.com>
2023-04-01 07:26:21 -0700

    TASK 0

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,8 +1,10 @@
 #include "main.h"
 #include <math.h>
+
 /**
   * is_prime_number - A function that calls is prime
   * @n: Number to be checked
+  * Return: 1 if prime 0 if not
   */
 int is_prime_number(int n)
 {
@@ -38,6 +40,7 @@ int is_prime(int n, int divisor)
 		return (is_prime(n, divisor + 1));
 	}
 }
+
 /**
   * _sqrt_recursion - A function that calls a square root function
   * @n: Number
@@ -45,7 +48,7 @@ int is_prime(int n, int divisor)
   */
 int _sqrt_recursion(int n)
 {
-        return (square_root(n, 0, n));
+	return (square_root(n, 0, n));
 }
 
 /**
@@ -57,26 +60,25 @@ int _sqrt_recursion(int n)
   */
 int square_root(int num, int low, int high)
 {
-        int mid;
-        int mid_squared;
+	int mid;
+	int mid_squared;
 
-        if (low > high)
-        {
-                return (-1);
-        }
-        mid = (low + high) / 2;
-        mid_squared = mid * mid;
-
-        if (mid_squared == num)
-        {
-                return (mid);
-        }
-        else if (mid_squared < num)
-        {
-                return (square_root(num, mid + 1, high));
-        }
-        else
-        {
-                return (square_root(num, low, mid - 1));
-        }
+	if (low > high)
+	{
+		return (-1);
+	}
+	mid = (low + high) / 2;
+	mid_squared = mid * mid;
+	if (mid_squared == num)
+	{
+		return (mid);
+	}
+	else if (mid_squared < num)
+	{
+		return (square_root(num, mid + 1, high));
+	}
+	else
+	{
+		return (square_root(num, low, mid - 1));
+	}
 }
