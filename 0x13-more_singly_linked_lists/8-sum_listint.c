0d0dc68bfda1a417792762803b46ade52e2022f0
mkmtde47 <mkmmogano@gmail.com>
2023-04-03 14:17:03 -0700

    TASK 8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+  * sum_listint - A function that sums all numbers innodes
+  * @head: Pointer to a list
+  * Return: sum
+  */
+int sum_listint(listint_t *head)
+{
+	listint_t *current;
+	int sum;
+
+	sum = 0;
+	if (head == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		current = head;
+		while (current != NULL)
+		{
+			sum = sum + current->n;
+			current = current->next;
+		}
+	}
+	return (sum);
+}
