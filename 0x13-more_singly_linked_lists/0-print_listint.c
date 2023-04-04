b30fc862f0539fa2a5d9c3bc40932db711fa6f9d
mkmtde47 <mkmmogano@gmail.com>
2023-04-04 01:59:49 -0700

    TASK 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,22 +8,21 @@
 size_t print_listint(const listint_t *h)
 {
 	int count;
-	const listint_t *current;
+	const listint_t *current = h;
 
 	count = 0;
 	if (h != NULL)
 	{
-		current = h;
-		while (h != NULL)
+		while (current != NULL)
 		{
-			printf("%d\n", h->n);
+			printf("%d\n", current->n);
 			current = current->next;
 			count++;
 		}
 	}
 	else
 	{
-		/*printf("(nil)\n");*/
+		printf("(nil)\n");
 		return (0);
 	}
 	return (count);
