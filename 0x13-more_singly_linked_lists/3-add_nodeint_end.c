fff72a00d4c106b9e6355522d699ac3f77d525d0
mkmtde47 <mkmmogano@gmail.com>
2023-04-04 05:27:08 -0700

    task 5

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -12,7 +12,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *current;
 
 	new = malloc(sizeof(listint_t));
-	if (new == NULL)
+	if (new == NULL || n < 0)
 	{
 		free(new);
 		return (NULL);
--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,7 +8,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *current;
 	listint_t *temp;
-	
+
 	temp = *head;
 	*head = NULL;
 	while (temp != NULL)
