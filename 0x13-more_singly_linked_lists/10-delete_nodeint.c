9c51cb16cf0d8b3a46792a43b0452a0376655370
mkmtde47 <mkmmogano@gmail.com>
2023-04-04 01:15:32 -0700

    task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
-  * insert_nodeint_at_index - A function that adds the nth node at n
+  * delete_nodeint_at_index - A function that adds the nth node at n
   * @head: Pointer to a pointer
   * @index: index of list
   * Return: 1 if successful, 0 if not
@@ -17,15 +17,22 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		return (-1);
 	}
+	else if (index == 0)
+	{
+		current = *head;
+		*head = current->next;
+		free(current);
+		return (1);
+	}
 	else
 	{
 		current = *head;
-		while (current != NULL && count <= index)
+		while (current != NULL && count < index)
 		{
 			if (count == index - 1)
 			{
-				temp = current;
-				current = temp->next;
+				temp = current->next;
+				current->next = current->next->next;
 				free(temp);
 				return (1);
 			}
