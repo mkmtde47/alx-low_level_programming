7b00bef0a260b240bd434ee0a0168e21d3f94e4f
mkmtde47 <mkmmogano@gmail.com>
2023-04-21 01:08:19 -0700

    Revert "Delete 0x0C-more_malloc_free directory"
    This reverts commit ddb554f11024ed72059b19389096c1249217c9e4.

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+  * string_nconcat - Function that concatenates two strings
+  * @s1: First string
+  * @s2: Second string
+  * @n: Length of second string
+  * Return: str if successful, Null if failed
+  */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *str;
+	unsigned int i;
+	unsigned int j;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	str = malloc(sizeof(char) * (strlen(s1) + 1) * (strlen(s2) + 1));
+	if (str == NULL)
+	{
+		free(str);
+		return (NULL);
+	}
+	for (i = 0; i < strlen(s1) + 1; i++)
+	{
+		str[i] = *(s1 + i);
+	}
+	i--;
+	if (n >= strlen(s2) + 1)
+	{
+		n = strlen(s2) + 1;
+	}
+	for (j = 0; j < n; j++)
+	{
+		str[i] = *(s2 + j);
+		i++;
+	}
+	return (str);
+}
---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+  * array_range - Function for a array
+  * @min: minimum value
+  * @max: maximum value
+  * Return: Pointer if succesful, Null if not
+  */
+int *array_range(int min, int max)
+{
+	int *arr;
+	unsigned int i;
+	unsigned int sum;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+	sum = max - min;
+	arr = malloc(sizeof(int) * sum + 1);
+	if (arr == NULL)
+	{
+		free(arr);
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < sum; i++)
+		{
+			arr[i] = min + i;
+		}
+	}
+	return (arr);
+}
