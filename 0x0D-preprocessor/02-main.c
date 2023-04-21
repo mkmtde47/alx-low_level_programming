a38ea12aba22ecedc1b82ce7dea756ad2e51c0e4
mkmtde47 <mkmmogano@gmail.com>
2023-04-21 01:07:57 -0700

    Revert "Delete 0x0D-preprocessor directory"
    This reverts commit 089e02beebe69671c1a90d196cced3680b2a0c40.

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+int main(void)
+{
+	printf("%s", __FILE__);
+	printf("\n");
+	return (0);
+}
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+  * main - Entry point
+  * Return: Zero if successful
+  */
+int main(void)
+{
+	printf("%s", __FILE__);
+	printf("\n");
+	return (0);
+}
