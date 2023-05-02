781c041c1becf17ece8456777ba74d823b0015cd
mkmtde47 <mkmmogano@gmail.com>
2023-05-02 10:39:05 -0700

    Linked list tasks

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+  * print_list - A function that prints elements of a linked list
+  * @h: Pointer to a node
+  * Return: size of list
+  */
+size_t print_list(const list_t *h)
+{
+	size_t i;
+
+	i = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)");
+			printf("\n");
+		}
+		else
+		{
+			printf("[%d] %s", h->len, h->str);
+			printf("\n");
+		}
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+  * print_first - A function that executes before main
+  */
+void print_first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
